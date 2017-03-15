a96d09f10bffc84110ff97b07f9589bf6c656599
Allan Denis <allancomll@gmail.com>
2017-03-15 09:36:47 -0300

    Bot funcionando. Falta reconhecimento de voz.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from gtts import gTTS
 from pygame import mixer
 from pprint import pprint
 import aiml
+import speech_recognition as sr
 
 def falar(texto, idioma='pt-BR'):
     som = "/tmp/tts.mp3"
@@ -22,22 +23,38 @@ def falar(texto, idioma='pt-BR'):
     except Exception as e:
         print('Erro: ' + str(e))
 
-# bot = aiml.Kernel()
-# bot.learn("basic.aiml")
-#
-# while True:
-#     try:
-#         resposta = bot.respond(raw_input("Você>\t"))
-#         falar(resposta)
-#         print('Bot>\t' + resposta)
-#     except KeyboardInterrupt:
-#         print('')
-#         resposta = bot.respond('ADEUS')
-#         falar(resposta)
-#         print(resposta)
-#         # exit(0)
-#     # except Exception as e:
-#     #     if(e.message == 'No text to speak'):
-#     #         print
+bot = aiml.Kernel()
+bot.learn("basic.aiml")
+'''
+while True:
+    try:
 
-while True: falar(raw_input())
+        resposta = bot.respond(raw_input("Você>\t"))
+        falar(resposta)
+        print('Bot>\t' + resposta)
+    except KeyboardInterrupt:
+        print('')
+        resposta = bot.respond('ADEUS')
+        falar(resposta)
+        print(resposta)
+'''
+        # exit(0)
+    # except Exception as e:
+    #     if(e.message == 'No text to speak'):
+    #         print
+
+# while True: falar(raw_input())
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+
+# recognize speech using Sphinx
+try:
+    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+except sr.UnknownValueError:
+    print("Sphinx could not understand audio")
+except sr.RequestError as e:
+    print("Sphinx error; {0}".format(e))
