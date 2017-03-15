8086ec10a74bdd55a625b3fc37b78e75e53cda3c
Allan Denis <allancomll@gmail.com>
2017-03-15 09:57:53 -0300

    Usando JSON factory

--- api.py
+++ api.py
@@ -2,7 +2,34 @@ from main import *
 from flask import Flask, jsonify
 from werkzeug.exceptions import default_exceptions, HTTPException
 
-app = Flask(__name__)
+__all__ = ['make_json_app']
+
+def make_json_app(import_name, **kwargs):
+    """
+    Creates a JSON-oriented Flask app.
+
+    (from: http://flask.pocoo.org/snippets/83/)
+    All error responses that you don't specifically
+    manage yourself will have application/json content
+    type, and will contain JSON like this (just an example):
+
+    { "message": "405: Method Not Allowed" }
+    """
+    def make_json_error(ex):
+        response = jsonify(message=str(ex))
+        response.status_code = (ex.code
+                                if isinstance(ex, HTTPException)
+                                else 500)
+        return response
+
+    app = Flask(import_name, **kwargs)
+
+    for code in default_exceptions.iterkeys():
+        app.error_handler_spec[None][code] = make_json_error
+
+    return app
+
+app = make_json_app(__name__)
 
 @app.route("/")
 def hello():
