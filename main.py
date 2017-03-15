f89b4cd7d332f7552cba90b888906f76cc40a905
Allan Denis <allancomll@gmail.com>
2017-03-15 18:56:28 -0300

    Bibliotecas de TTS e STT removidas

--- main.py
+++ main.py
@@ -1,29 +1,11 @@
 from pprint import pprint
 import aiml
 
-def falar(texto, idioma='pt-BR'):
-    som = "/tmp/tts.mp3"
-    # print("Convertendo texto para voz: %s\nAguarde..." % texto)
-    tts = ''
-    try:
-        tts = gTTS(text=texto, lang=idioma)
-        tts.save(som)
-    except Exception as e:
-        pprint(e.message)
-
-    try:
-        mixer.init()
-            # print("Reproduzindo voz...")
-        mixer.music.load(som)
-        mixer.music.play()
-    except Exception as e:
-        print('Erro: ' + str(e))
-
 bot = aiml.Kernel()
-# bot.learn("bot.xml")
 bot.learn("aiml/personality.aiml")
 
 def cli():
+"""Interface de linha de comando para testar o bot."""
     while True:
         try:
             resposta = bot.respond(input("Você>\t"))
